8f344682f680c451f0aea34a078d7b190a6f0d79
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-05-26 16:31:28 -0500

    Time: 51 ms (62.73%), Space: 14.3 MB (56.99%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,63 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        '''
+        Find the pivot index -> O(lgn)
+        rotate the array around pivot -> O() should be able to do it in constant time
+        Find the element in the sorted array -> O(lgn)
+        
+        '''
+        
+        start = 0
+        end = len(nums) - 1
+        pivot_index = 0
+        mid = end - (end-start)//2
+        while(end>start):
+            mid = end - (end - start)//2
+            """
+            check if mid value is pivot
+            """
+            if(nums[mid-1] > nums[mid] or nums[mid] > nums[mid] > nums[mid+1]):
+                pivot_index = mid
+                break
+            """
+            if not see if the pivot is in first half or the second half
+            """
+            if(nums[start]>nums[mid]):
+                """
+                Pivot is in upper half
+                """
+                end = mid
+                pass
+            elif nums[end]<nums[mid]:
+                """
+                Pivot is in lower half
+                """
+                start = mid
+            else:
+                """
+                there are not pivot in the array
+                """
+                break
+        print(pivot_index)
+        start = 0
+        end = len(nums) - 1
+        mid = end - (end-start)//2
+        while(end-start>1):
+            
+            print("start: ", (start+pivot_index)%len(nums))
+            print("End: ", (end+pivot_index)%len(nums))
+            print("Mid: ", (mid+pivot_index)%len(nums))
+            if(target == nums[(mid+pivot_index)%len(nums)]):
+                return (mid+pivot_index)%len(nums)
+            elif target > nums[(mid+pivot_index)%len(nums)]:
+                start = mid
+            else:
+                end = mid
+            mid = end - (end-start)//2
+        if(nums[(start+pivot_index)%len(nums)] == target):
+            return (start+pivot_index)%len(nums)
+        if(nums[(end+pivot_index)%len(nums)] == target):
+            return (end+pivot_index)%len(nums)
+        if nums[(mid+pivot_index)%len(nums)] == target:
+            return (mid+pivot_index)%len(nums)
+        return -1
