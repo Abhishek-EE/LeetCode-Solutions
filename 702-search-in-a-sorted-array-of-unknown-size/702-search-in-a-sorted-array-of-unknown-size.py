06950f82a6d4d43aafd70a0992e7df07037f493e
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-05-30 15:07:29 -0500

    Time: 71 ms (16.86%), Space: 15 MB (95.74%) - LeetHub

--- 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.py
+++ 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.py
@@ -5,19 +5,42 @@
 #class ArrayReader:
 #    def get(self, index: int) -> int:
 
+# class Solution:
+#     def search(self, reader: 'ArrayReader', target: int) -> int:
+#         '''
+#         You could actually do binary search in the 10^4 space which will solve the problem in O(10^4) complexity
+#         but it will always take that much time which is constant but is almsot always worst case
+#         '''
+#         start = 0
+#         end = pow(10,4)
+#         while end-start>=0:
+#             mid = start + (end-start)//2
+#             print("Start: ", start)
+#             print("Mid: ",mid)
+#             print("End", end)
+#             if(reader.get(mid) == target):
+#                 return mid
+#             elif reader.get(mid) < target:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#         return -1
+
+
 class Solution:
     def search(self, reader: 'ArrayReader', target: int) -> int:
         '''
         You could actually do binary search in the 10^4 space which will solve the problem in O(10^4) complexity
         but it will always take that much time which is constant but is almsot always worst case
         '''
+        #It is quintessential to find the pivot
         start = 0
-        end = pow(10,4)
-        while end-start>=0:
-            mid = start + (end-start)//2
-            print("Start: ", start)
-            print("Mid: ",mid)
-            print("End", end)
+        end = 1
+        while reader.get(end) < target:
+            start = end
+            end = 2*end
+        while(end-start>=0):
+            mid = end - (end-start)//2
             if(reader.get(mid) == target):
                 return mid
             elif reader.get(mid) < target:
