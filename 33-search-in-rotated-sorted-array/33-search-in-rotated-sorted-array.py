69f90d7d94aada767cbf5b709b64f7c3a63ca910
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-05-30 16:13:41 -0500

    Time: 74 ms (19.57%), Space: 14.2 MB (91.38%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,63 +1,51 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        '''
-        Find the pivot index -> O(lgn)
-        rotate the array around pivot -> O() should be able to do it in constant time
-        Find the element in the sorted array -> O(lgn)
-        
-        '''
-        
+        """
+        brute force -> traverse the array in O(N) find the location
+        O(1)
+        Approach is itterative -> additional space is o(1)
+        find the pivot in the array using binary search O(lgn)
+        one the pivot is found it is very simple to find 
+        """
         start = 0
         end = len(nums) - 1
-        pivot_index = 0
-        mid = end - (end-start)//2
-        while(end>start):
-            mid = end - (end - start)//2
-            """
-            check if mid value is pivot
-            """
-            if(nums[mid-1] > nums[mid]):
-                pivot_index = mid
+        pivot = 0
+        while end-start>=0:
+            mid = start + (end-start)//2
+            #if the current element is pivot make pivot = mid and break
+            if nums[mid-1] > nums[mid]:
+                pivot = mid
                 break
-            """
-            if not see if the pivot is in first half or the second half
-            """
-            if(nums[start]>nums[mid]):
-                """
-                Pivot is in upper half
-                """
-                end = mid
-                pass
-            elif nums[end]<nums[mid]:
-                """
-                Pivot is in lower half
-                """
-                start = mid
+            #else if the mid element is greater than end element unsorted array is in later half make start = mid + 1
+            elif nums[mid] > nums[end]:
+                start = mid+1
+            #else if the mid elmennt is smaller than the start element unsorted array is in upper half make end = mid - 1
+            elif nums[mid] < nums[start]:
+                end = mid - 1
+            # else the array is perfectly sorted and pivot is basically start of this array
             else:
-                """
-                there are not pivot in the array
-                """
+                pivot = start
                 break
-        print(pivot_index)
+        """
+        if the target element is greater than pivot index
+        need to look from pivot index to end
+        if the target elem
+        """
+        print(pivot)
         start = 0
         end = len(nums) - 1
-        mid = end - (end-start)//2
-        while(end-start>1):
-            
-            print("start: ", (start+pivot_index)%len(nums))
-            print("End: ", (end+pivot_index)%len(nums))
-            print("Mid: ", (mid+pivot_index)%len(nums))
-            if(target == nums[(mid+pivot_index)%len(nums)]):
-                return (mid+pivot_index)%len(nums)
-            elif target > nums[(mid+pivot_index)%len(nums)]:
-                start = mid
+        if target >= nums[pivot] and target<=nums[end]:
+            start = pivot
+        else:
+            start = 0
+            end = pivot - 1
+        while end-start>=0:
+            mid = start + (end-start)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
             else:
-                end = mid
-            mid = end - (end-start)//2
-        if(nums[(start+pivot_index)%len(nums)] == target):
-            return (start+pivot_index)%len(nums)
-        if(nums[(end+pivot_index)%len(nums)] == target):
-            return (end+pivot_index)%len(nums)
-        if nums[(mid+pivot_index)%len(nums)] == target:
-            return (mid+pivot_index)%len(nums)
+                end = mid - 1
+                
         return -1
