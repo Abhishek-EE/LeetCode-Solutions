fb92febf68fb8f7834bee6016804574e2bb95fff
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-06-01 23:20:41 -0500

    Time: 40 ms (66.63%), Space: 13.8 MB (54.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n==1:
-            return n
-        return self.fib(n-1) + self.fib(n-2) 
+        """
+        The way to solve a fibonachi problem is
+        come up with a recursive solution
+        store the answer of subproblem in a hash-map
+        reutrn 
+        you could do it recursively but let me first do it non recursively
+        """
+        A = [0,1]
+        for i in range(2,n+1):
+            A.append(A[i-1] + A[i-2])
+        print(A)
+        return A[n]
+        
         
