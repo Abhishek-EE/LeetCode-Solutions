3bb6a9c8c490ba9cc982fb7a9728714fc27dbc7b
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-06-05 23:25:02 -0500

    Time: 62 ms (12.23%), Space: 13.9 MB (19.04%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -2,19 +2,14 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         """The way I feel like we can solve it is you can either rob even houses or odd houses
         see which one has the max, just keep """
-        mem = collections.defaultdict(int)
-        def robFrom(i):
-            if i > len(nums) -2 and i<len(nums)-1:
-                mem[i] = nums[i]
-                return nums[i]
-            if i>len(nums)-1:
-                mem[i] = 0
-                return 0
-            if i in mem:
-                return mem[i]
-            else:
-                answer = max(robFrom(i+1),robFrom(i+2)+nums[i])
-                mem[i] = answer
-            return answer
-        answer = robFrom(0)
-        return answer
+        mem = [-1 for i in range(len(nums)+1)]
+        if(len(nums)<3):
+            return max(nums)
+        mi2 = 0
+        mi1 = nums[len(nums)-1]
+        for i in reversed(range(len(nums)-1)):
+            mi = max(mi1,mi2+nums[i])
+            mi2 = mi1
+            mi1 = mi
+        return mi
+            
