5f4bb423e9a41e6e3c807b137314a891afe9f728
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-06-06 17:19:22 -0500

    Time: 47 ms (45.95%), Space: 14.3 MB (7.21%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        mem = collections.defaultdict(int)
+        def decoding(str1):
+            """
+            if the first element is greater
+            """
+            if str1 in mem:
+                return mem[str1]
+            if len(str1) == 0:
+                return 1
+            if int(str1[0]) == 0:
+                return 0
+            if len(str1) < 2:
+                return 1
+            ans = decoding(str1[1:])
+            if int(str1[0:2]) <= 26:
+                ans += decoding(str1[2:])
+            mem[str1] = ans
+            return ans
+        answer = decoding(s)
+        return answer
+        
+                
+            
+                
+                
+        
+            
+        
