def1a7452439540876b9b6a47ee9237b3ba7dfc9
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-06-24 15:55:47 -0500

    Time: 1987 ms (32.49%), Space: 14.2 MB (16.88%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        """
+        s_i = 0
+        e_j = 0
+        for i in range(len(s)):
+            for j in range(i,i+2):
+                if j > len(s) - 1:
+                    continue
+                print("question: ",i,j)
+                curr_i,curr_j = self.expandAroundCenter(i,j,s)
+                print("answer: ",curr_i,curr_j)
+                if curr_j - curr_i + 1 > e_j - s_i + 1:
+                    s_i = curr_i
+                    e_j = curr_j
+        return s[s_i:e_j+1]
+    
+    def expandAroundCenter(self,i,j,s):
+        ans_i = i
+        ans_j = i
+        while i>=0 and j<len(s):
+            if s[i] == s[j]:
+                ans_i = i
+                ans_j = j
+            else:
+                break
+            i -= 1
+            j += 1
+        
+        return (ans_i,ans_j)
+                
+                
+                
+                
