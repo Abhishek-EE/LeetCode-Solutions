8bc7a45785a9c6812c7cb147cb45a96412350942
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-09-11 00:22:56 -0500

    Time: 205 ms (5.09%), Space: 17.9 MB (12.29%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        print(intervals)
+        rooms_available = []
+        room_not_available = True
+        for i in range(len(intervals)):
+            for j in range(len(rooms_available)):
+                if intervals[i][0] >= rooms_available[j]:
+                    rooms_available[j] = intervals[i][1]
+                    room_not_available = False
+                    break
+            if room_not_available:
+                rooms_available.append(intervals[i][1])
+            room_not_available = True
+        return len(rooms_available)
+                
+        
