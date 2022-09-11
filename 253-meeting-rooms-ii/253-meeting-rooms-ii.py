f9beea55bf55a71889fa31664f64f967b20738e2
Abhishek Ranjan Singh <59440480+Abhishek-EE@users.noreply.github.com>
2022-09-11 00:44:39 -0500

    Time: 196 ms (5.09%), Space: 17.5 MB (50.81%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        rooms_available = []
-        room_not_available = True
-        for i in range(len(intervals)):
-            for j in range(len(rooms_available)):
-                if intervals[i][0] >= rooms_available[j]:
-                    rooms_available[j] = intervals[i][1]
-                    room_not_available = False
-                    break
-            if room_not_available:
-                rooms_available.append(intervals[i][1])
-            room_not_available = True
-        return len(rooms_available)
+        rooms_available = [intervals[0][1]]
+        heapify(rooms_available)
+        num_of_rooms = 1
+        # room_not_available = True
+        for i in range(1,len(intervals)):
+            earliest_available_room = rooms_available[0]
+            if intervals[i][0] >= earliest_available_room:
+                heapreplace(rooms_available,intervals[i][1])
+            else:
+                heappush(rooms_available,intervals[i][1])
+                num_of_rooms += 1
+        
+        return num_of_rooms
                 
         
